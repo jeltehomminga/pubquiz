0f255039bc02ce2cc53870f40e10d712b0ea5413
Jelte Homminga <j.homminga@hypotheekbond.nl>
2020-03-30 07:40:08 +0200

    add some more style 👨🏼‍🎨

--- src/components/elements.js
+++ src/components/elements.js
@@ -2,7 +2,8 @@ import styled from "@emotion/styled";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
-export const Btn = styled.button({
+
+const buttonStyle = {
   fontSize: 16,
   padding: 8,
   width: 170,
@@ -19,26 +20,16 @@ export const Btn = styled.button({
     transform: "translateY(-0.1rem)",
     transition: "transform 150ms"
   }
+}
+
+export const Btn = styled.button({
+  ...buttonStyle,
+  marginRight: 16, 
 });
 
 export const LinkButton = styled(Link)({
+  ...buttonStyle,
   margin: "0 auto",
-  fontSize: 16,
-  padding: 8,
-  width: 170,
-  textAlign: "center",
-  border: "0.1rem solid #1f1e32",
-  marginBottom: "16px",
-  color: "#1f1e32",
-  textDecoration: "none",
-  backgroundColor: "white",
-
-  ":hover": {
-    cursor: "pointer",
-    boxShadow: "0 0.4rem 1.4rem 0 rgba(13, 36, 48, 0.5)",
-    transform: "translateY(-0.1rem)",
-    transition: "transform 150ms"
-  }
 });
 
 export const FlexContainer = styled.div(({ column }) => ({
@@ -58,5 +49,6 @@ export const CenteredDiv = styled(motion.div)({
   margin: 15,
   display: "flex",
   flexDirection: "column",
-  justifyContent: "space-around"
+  justifyContent: "space-around",
+  boxShadow: '10px 10px 20px rgba(0, 0, 0, 0.4)'
 });
